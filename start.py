11af092b98ef7b39266cb7d7a89b1fd5f906789f
alice <tai45612360@gmail.com>
2022-09-28 09:02:11 +0800

    Add files via upload
    test1

---
+++ start.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtWidgets
+from mainController import MainController
+import os
+
+if __name__ == '__main__':
+    # confirm the folder first  # file can build directly
+    if not os.path.exists('gitignore'):os.mkdir('gitignore')
+    # f= open("/gitignore/cookie.txt","x")
+    # if os.path.isfile('/gitignore/cookie.txt'):os.mknod('/gitignore/cookie.txt')
+
+    # perform main
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainController()
+    window.show()
+    sys.exit(app.exec_())
